e838de977c0baf0f8377d5ad8ae2b7d8b5ec15e0
Miles Davis <mrdavis1997gmail.com>
2021-04-01 20:21:56 -0500

    Made JavaScript file and added src to contact page

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Script for hamburger menu on mobile */
+// Set the width of the side navigation to 250px
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.borderWidth = "4px";
+  document.getElementById("dim").style.display = "block";
+}
+
+// Set the width of the side navigation to 0
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.borderWidth = "0px";
+  document.getElementById("dim").style.display = "none";
+}
